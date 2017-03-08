f472d2d1e26ebda0c13fcf5fdaba8e4da0bf3fc4
Louis Laagland <louis@wunderful.nl>
2017-03-08 15:16:26 +0100

    Add serveSecure method for using HttpServer.bindSecure to serve SSL

--- lib/shelf_io.dart
+++ lib/shelf_io.dart
@@ -40,175 +40,204 @@ Future<HttpServer> serve(Handler handler, address, int port, {int backlog}) {
   });
 }
 
-/// Serve a [Stream] of [HttpRequest]s.
+/// Starts an [HttpServer] that listens on the specified [address] and
+/// [port] using SSL/TLS and sends requests to [handler].
 ///
-/// [HttpServer] implements [Stream<HttpRequest>] so it can be passed directly
-/// to [serveRequests].
-///
-/// Errors thrown by [handler] while serving a request will be printed to the
-/// console and cause a 500 response with no body. Errors thrown asynchronously
-/// by [handler] will be printed to the console or, if there's an active error
-/// zone, passed to that zone.
-void serveRequests(Stream<HttpRequest> requests, Handler handler) {
-  catchTopLevelErrors(() {
-    requests.listen((request) => handleRequest(request, handler));
-  }, (error, stackTrace) {
-    _logTopLevelError('Asynchronous error\n$error', stackTrace);
-  });
-}
+/// See the documentation for [HttpServer.bindSecure] for more details on [address],
+/// [port], [backlog] and [certificateName].
+//Future<HttpServer> serveSecure(Handler handler, address, int port,
+//    {int backlog, bool requestClientCertificate: false}) {
 
-/// Uses [handler] to handle [request].
-///
-/// Returns a [Future] which completes when the request has been handled.
-Future handleRequest(HttpRequest request, Handler handler) async {
-  var shelfRequest;
-  try {
-    shelfRequest = _fromHttpRequest(request);
-  } catch (error, stackTrace) {
-    var response = _logTopLevelError(
-        'Error parsing request.\n$error', stackTrace);
-    await _writeResponse(response, request.response);
-    return;
+Future<HttpServer> serveSecure(Handler handler, address, int port, SecurityContext context,
+    {int backlog, bool v6Only, bool requestClientCertificate, bool shared}) {
+
+    if (backlog == null) backlog = 0;
+    if (v6Only == null) v6Only = false;
+    if (requestClientCertificate == null) requestClientCertificate = false;
+    if (shared == null) shared = false;
+
+    return HttpServer.bindSecure(address,
+      port,
+      context: context,
+      backlog: backlog,
+      v6Only: v6Only,
+      requestClientCertificate: requestClientCertificate,
+      shared: shared).then((server) {
+      serveRequests(server, handler);
+      return server;
+    });
   }
 
-  // TODO(nweiz): abstract out hijack handling to make it easier to implement an
-  // adapter.
-  var response;
-  try {
-    response = await handler(shelfRequest);
-  } on HijackException catch (error, stackTrace) {
-    // A HijackException should bypass the response-writing logic entirely.
-    if (!shelfRequest.canHijack) return;
-
-    // If the request wasn't hijacked, we shouldn't be seeing this exception.
-    response = _logError(
-        shelfRequest,
-        "Caught HijackException, but the request wasn't hijacked.",
-        stackTrace);
-  } catch (error, stackTrace) {
-    response = _logError(
-        shelfRequest, 'Error thrown by handler.\n$error', stackTrace);
+  /// Serve a [Stream] of [HttpRequest]s.
+  ///
+  /// [HttpServer] implements [Stream<HttpRequest>] so it can be passed directly
+  /// to [serveRequests].
+  ///
+  /// Errors thrown by [handler] while serving a request will be printed to the
+  /// console and cause a 500 response with no body. Errors thrown asynchronously
+  /// by [handler] will be printed to the console or, if there's an active error
+  /// zone, passed to that zone.
+  void serveRequests(Stream<HttpRequest> requests, Handler handler) {
+    catchTopLevelErrors(() {
+      requests.listen((request) => handleRequest(request, handler));
+    }, (error, stackTrace) {
+      _logTopLevelError('Asynchronous error\n$error', stackTrace);
+    });
   }
 
-  if (response == null) {
-    await _writeResponse(
-        _logError(shelfRequest, 'null response from handler.'),
-        request.response);
-    return;
-  } else if (shelfRequest.canHijack) {
-    await _writeResponse(response, request.response);
-    return;
+  /// Uses [handler] to handle [request].
+  ///
+  /// Returns a [Future] which completes when the request has been handled.
+  Future handleRequest(HttpRequest request, Handler handler) async {
+    var shelfRequest;
+    try {
+      shelfRequest = _fromHttpRequest(request);
+    } catch (error, stackTrace) {
+      var response = _logTopLevelError(
+          'Error parsing request.\n$error', stackTrace);
+      await _writeResponse(response, request.response);
+      return;
+    }
+
+    // TODO(nweiz): abstract out hijack handling to make it easier to implement an
+    // adapter.
+    var response;
+    try {
+      response = await handler(shelfRequest);
+    } on HijackException catch (error, stackTrace) {
+      // A HijackException should bypass the response-writing logic entirely.
+      if (!shelfRequest.canHijack) return;
+
+      // If the request wasn't hijacked, we shouldn't be seeing this exception.
+      response = _logError(
+          shelfRequest,
+          "Caught HijackException, but the request wasn't hijacked.",
+          stackTrace);
+    } catch (error, stackTrace) {
+      response = _logError(
+          shelfRequest, 'Error thrown by handler.\n$error', stackTrace);
+    }
+
+    if (response == null) {
+      await _writeResponse(
+          _logError(shelfRequest, 'null response from handler.'),
+          request.response);
+      return;
+    } else if (shelfRequest.canHijack) {
+      await _writeResponse(response, request.response);
+      return;
+    }
+
+    var message = new StringBuffer()
+      ..writeln("Got a response for hijacked request "
+          "${shelfRequest.method} ${shelfRequest.requestedUri}:")
+      ..writeln(response.statusCode);
+    response.headers
+        .forEach((key, value) => message.writeln("${key}: ${value}"));
+    throw new Exception(message.toString().trim());
   }
 
-  var message = new StringBuffer()
-    ..writeln("Got a response for hijacked request "
-        "${shelfRequest.method} ${shelfRequest.requestedUri}:")
-    ..writeln(response.statusCode);
-  response.headers
-      .forEach((key, value) => message.writeln("${key}: ${value}"));
-  throw new Exception(message.toString().trim());
-}
+  /// Creates a new [Request] from the provided [HttpRequest].
+  Request _fromHttpRequest(HttpRequest request) {
+    var headers = <String, String>{};
+    request.headers.forEach((k, v) {
+      // Multiple header values are joined with commas.
+      // See http://tools.ietf.org/html/draft-ietf-httpbis-p1-messaging-21#page-22
+      headers[k] = v.join(',');
+    });
 
-/// Creates a new [Request] from the provided [HttpRequest].
-Request _fromHttpRequest(HttpRequest request) {
-  var headers = <String, String>{};
-  request.headers.forEach((k, v) {
-    // Multiple header values are joined with commas.
-    // See http://tools.ietf.org/html/draft-ietf-httpbis-p1-messaging-21#page-22
-    headers[k] = v.join(',');
-  });
+    // Remove the Transfer-Encoding header per the adapter requirements.
+    headers.remove(HttpHeaders.TRANSFER_ENCODING);
 
-  // Remove the Transfer-Encoding header per the adapter requirements.
-  headers.remove(HttpHeaders.TRANSFER_ENCODING);
+    void onHijack(callback) {
+      request.response
+          .detachSocket(writeHeaders: false)
+          .then((socket) => callback(socket, socket));
+    }
 
-  void onHijack(callback) {
-    request.response
-        .detachSocket(writeHeaders: false)
-        .then((socket) => callback(socket, socket));
+    return new Request(request.method, request.requestedUri,
+        protocolVersion: request.protocolVersion,
+        headers: headers,
+        body: request,
+        onHijack: onHijack);
   }
 
-  return new Request(request.method, request.requestedUri,
-      protocolVersion: request.protocolVersion,
-      headers: headers,
-      body: request,
-      onHijack: onHijack);
-}
+  Future _writeResponse(Response response, HttpResponse httpResponse) {
+    if (response.context.containsKey("shelf.io.buffer_output")) {
+      httpResponse.bufferOutput = response.context["shelf.io.buffer_output"];
+    }
 
-Future _writeResponse(Response response, HttpResponse httpResponse) {
-  if (response.context.containsKey("shelf.io.buffer_output")) {
-    httpResponse.bufferOutput = response.context["shelf.io.buffer_output"];
+    httpResponse.statusCode = response.statusCode;
+
+    // An adapter must not add or modify the `Transfer-Encoding` parameter, but
+    // the Dart SDK sets it by default. Set this before we fill in
+    // [response.headers] so that the user or Shelf can explicitly override it if
+    // necessary.
+    httpResponse.headers.chunkedTransferEncoding = false;
+
+    response.headers.forEach((header, value) {
+      if (value == null) return;
+      httpResponse.headers.set(header, value);
+    });
+
+    var coding = response.headers['transfer-encoding'];
+    if (coding != null && !equalsIgnoreAsciiCase(coding, 'identity')) {
+      // If the response is already in a chunked encoding, de-chunk it because
+      // otherwise `dart:io` will try to add another layer of chunking.
+      //
+      // TODO(nweiz): Do this more cleanly when sdk#27886 is fixed.
+      response = response.change(
+          body: chunkedCoding.decoder.bind(response.read()));
+      httpResponse.headers.set(HttpHeaders.TRANSFER_ENCODING, 'chunked');
+    } else if (response.statusCode >= 200 &&
+        response.statusCode != 204 &&
+        response.statusCode != 304 &&
+        response.contentLength == null &&
+        response.mimeType != 'multipart/byteranges') {
+      // If the response isn't chunked yet and there's no other way to tell its
+      // length, enable `dart:io`'s chunked encoding.
+      httpResponse.headers.set(HttpHeaders.TRANSFER_ENCODING, 'chunked');
+    }
+
+    if (!response.headers.containsKey(HttpHeaders.SERVER)) {
+      httpResponse.headers.set(HttpHeaders.SERVER, 'dart:io with Shelf');
+    }
+
+    if (!response.headers.containsKey(HttpHeaders.DATE)) {
+      httpResponse.headers.date = new DateTime.now().toUtc();
+    }
+
+    return httpResponse
+        .addStream(response.read())
+        .then((_) => httpResponse.close());
   }
-
-  httpResponse.statusCode = response.statusCode;
-
-  // An adapter must not add or modify the `Transfer-Encoding` parameter, but
-  // the Dart SDK sets it by default. Set this before we fill in
-  // [response.headers] so that the user or Shelf can explicitly override it if
-  // necessary.
-  httpResponse.headers.chunkedTransferEncoding = false;
-
-  response.headers.forEach((header, value) {
-    if (value == null) return;
-    httpResponse.headers.set(header, value);
-  });
-
-  var coding = response.headers['transfer-encoding'];
-  if (coding != null && !equalsIgnoreAsciiCase(coding, 'identity')) {
-    // If the response is already in a chunked encoding, de-chunk it because
-    // otherwise `dart:io` will try to add another layer of chunking.
-    //
-    // TODO(nweiz): Do this more cleanly when sdk#27886 is fixed.
-    response = response.change(
-        body: chunkedCoding.decoder.bind(response.read()));
-    httpResponse.headers.set(HttpHeaders.TRANSFER_ENCODING, 'chunked');
-  } else if (response.statusCode >= 200 &&
-      response.statusCode != 204 &&
-      response.statusCode != 304 &&
-      response.contentLength == null &&
-      response.mimeType != 'multipart/byteranges') {
-    // If the response isn't chunked yet and there's no other way to tell its
-    // length, enable `dart:io`'s chunked encoding.
-    httpResponse.headers.set(HttpHeaders.TRANSFER_ENCODING, 'chunked');
-  }
-
-  if (!response.headers.containsKey(HttpHeaders.SERVER)) {
-    httpResponse.headers.set(HttpHeaders.SERVER, 'dart:io with Shelf');
-  }
-
-  if (!response.headers.containsKey(HttpHeaders.DATE)) {
-    httpResponse.headers.date = new DateTime.now().toUtc();
-  }
-
-  return httpResponse
-      .addStream(response.read())
-      .then((_) => httpResponse.close());
-}
 
 // TODO(kevmoo) A developer mode is needed to include error info in response
 // TODO(kevmoo) Make error output plugable. stderr, logging, etc
-Response _logError(Request request, String message, [StackTrace stackTrace]) {
-  // Add information about the request itself.
-  var buffer = new StringBuffer();
-  buffer.write("${request.method} ${request.requestedUri.path}");
-  if (request.requestedUri.query.isNotEmpty) {
-    buffer.write("?${request.requestedUri.query}");
+  Response _logError(Request request, String message, [StackTrace stackTrace]) {
+    // Add information about the request itself.
+    var buffer = new StringBuffer();
+    buffer.write("${request.method} ${request.requestedUri.path}");
+    if (request.requestedUri.query.isNotEmpty) {
+      buffer.write("?${request.requestedUri.query}");
+    }
+    buffer.writeln();
+    buffer.write(message);
+
+    return _logTopLevelError(buffer.toString(), stackTrace);
   }
-  buffer.writeln();
-  buffer.write(message);
 
-  return _logTopLevelError(buffer.toString(), stackTrace);
-}
+  Response _logTopLevelError(String message, [StackTrace stackTrace]) {
+    var chain = new Chain.current();
+    if (stackTrace != null) {
+      chain = new Chain.forTrace(stackTrace);
+    }
+    chain = chain
+        .foldFrames((frame) => frame.isCore || frame.package == 'shelf').terse;
 
-Response _logTopLevelError(String message, [StackTrace stackTrace]) {
-  var chain = new Chain.current();
-  if (stackTrace != null) {
-    chain = new Chain.forTrace(stackTrace);
+    stderr.writeln('ERROR - ${new DateTime.now()}');
+    stderr.writeln(message);
+    stderr.writeln(chain);
+    return new Response.internalServerError();
   }
-  chain = chain
-      .foldFrames((frame) => frame.isCore || frame.package == 'shelf').terse;
-
-  stderr.writeln('ERROR - ${new DateTime.now()}');
-  stderr.writeln(message);
-  stderr.writeln(chain);
-  return new Response.internalServerError();
 }
